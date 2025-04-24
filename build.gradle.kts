c1f3e2d4922e05f756b80e693ac7614b43128906
Ferdinand Lange <fl45gibe@studserv.uni-leipzig.de>
2025-04-24 13:25:15 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("org.jetbrains.kotlinx.dataframe") version "0.15.0"
+}
+
+group = "masterthesis"
+version = "1.0-SNAPSHOT"
+
+val dataframeVersion = "0.15.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:dataframe:$dataframeVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
